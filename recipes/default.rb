f2c1d00ecb256eb1a7fd61f7a5766d2f12d6a5a1
Riley Seaman <riley@foreflight.com>
2017-11-03 14:07:31 -0500

    Making web nodes dynamic in haproxy pool

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,17 +4,19 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-node.default['haproxy']['members'] = [{
-  'hostname' => 'ec2-54-84-18-114.compute-1.amazonaws.com',
-  'ipaddress' => '54.84.18.114',
-  'port' => 80,
-  'ssl_port' => 80
-},
-{
-  'hostname' => 'ec2-107-21-197-213.compute-1.amazonaws.com',
-  'ipaddress' => '107.21.197.213',
-  'port' => 80,
-  'ssl_port' => 80
-}]
+all_web_nodes = search('node', 'role:web_server')
+members = []
+
+all_web_nodes.each do |web_node|
+  member = {
+    'hostname' => web_node['cloud']['public_hostname'],
+    'ipaddress' => web_node['cloud']['public_ipv4'],
+    'port' => 80,
+    'ssl_port' => 80
+  }
+  members.push(member)
+end
+
+node.default['haproxy']['members'] = members
 
 include_recipe 'haproxy::manual'
