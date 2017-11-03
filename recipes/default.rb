ddeb8f8cc1b8768b99d701ba1a9034a94b6dd7a9
Riley Seaman <riley@foreflight.com>
2017-11-03 09:21:26 -0500

    Initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [{
+  'hostname' => 'ec2-54-84-18-114.compute-1.amazonaws.com',
+  'ipaddress' => '54.84.18.114',
+  'port' => 80,
+  'ssl_port' => 80
+}]
+
+include_recipe 'haproxy::manual'
